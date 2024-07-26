81601b160b5e61261beac5e7afc597d1f28e58b3
Martin Caslavsky <mcaslavsky@gmail.com>
2024-07-26 15:12:17 +0200

    chg: use assertj

--- src/test/java/cz/geek/OptionsTest.java
+++ src/test/java/cz/geek/OptionsTest.java
@@ -2,28 +2,27 @@ package cz.geek;
 
 import org.junit.jupiter.api.Test;
 
-import static java.util.Arrays.asList;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class OptionsTest {
 
     @Test
     void shouldAcceptRequiredArguments() throws Exception {
         Options options = Options.parse(System.out, "-u", "user", "-p", "pass");
-        assertEquals("user", options.getUser());
-        assertEquals("pass", options.getPassword());
-        assertEquals("imap.gmail.com", options.getHost());
-        assertNull(options.getLabel());
-        assertTrue(options.getArgs().isEmpty());
+        assertThat(options.getUser()).isEqualTo("user");
+        assertThat(options.getPassword()).isEqualTo("pass");
+        assertThat(options.getHost()).isEqualTo("imap.gmail.com");
+        assertThat(options.getLabel()).isNull();
+        assertThat(options.getArgs()).isEmpty();
     }
 
     @Test
     void shouldAcceptArguments() throws Exception {
         Options options = Options.parse(System.out, "-u", "user", "-p", "pass", "-h", "host", "-l", "label", "file1", "file2");
-        assertEquals("user", options.getUser());
-        assertEquals("pass", options.getPassword());
-        assertEquals("host", options.getHost());
-        assertEquals("label", options.getLabel());
-        assertEquals(asList("file1", "file2"), options.getArgs());
+        assertThat(options.getUser()).isEqualTo("user");
+        assertThat(options.getPassword()).isEqualTo("pass");
+        assertThat(options.getHost()).isEqualTo("host");
+        assertThat(options.getLabel()).isEqualTo("label");
+        assertThat(options.getArgs()).containsExactly("file1", "file2");
     }
 }
